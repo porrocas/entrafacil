ca8108302681aec9ea99ba4576904e5df7b24da7
Daniel Mateus <u1802833@unimilitar.edu.co>
2020-02-19 01:15:50 -0500

    [add] add functions link and add pages links

--- views/header.php
+++ views/header.php
@@ -1,12 +1,12 @@
 <!-- Image and text -->
 <nav id="navRedes" class="navbar navbar-light bg-success">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="https://web.facebook.com/entra.facil.73">
         FaceBook
     </a>
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="https://www.instagram.com/entrafacil/?hl=es-la">
         Instagram
     </a>
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="https://api.whatsapp.com/send?phone=+573213915844&text=Hola,%20EntraF%C3%A1cil.com%20Necesito%20Tus%20Servicios">
         Whatsapp
     </a>
     <a class="navbar-brand" href="#">
@@ -15,7 +15,7 @@
 </nav>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand tituloLogo" href="#">Entra <span>|Fácil.com</span> </a>
+    <a class="navbar-brand tituloLogo" href="index.php">Entra <span>|Fácil.com</span> </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -24,10 +24,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Eventos</a>
+                <a class="nav-link" href="eventos.php">Eventos</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
@@ -35,11 +35,11 @@
                     Menu
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Galeria</a>
-                    <a class="dropdown-item" href="#">Contacto</a>
-                    <a class="dropdown-item" href="#">About Us</a>
+                    <a class="dropdown-item" href="aboutUs.php#galeria">Galería</a>
+                    <a class="dropdown-item" href="aboutUs.php#contacto">Contacto</a>
+                    <a class="dropdown-item" href="aboutUs.php">About Us</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Soporte</a>
+                    <a class="dropdown-item" href="soporte.php">Soporte</a>
                 </div>
             </li>
 
@@ -49,7 +49,7 @@
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item ml-2">
-                    <a class="nav-link" href="#">Hola, Identificate!</a>
+                    <a class="nav-link" href="admin.php">Hola, Identificate!</a>
                 </li>
             </ul>
         </form>
