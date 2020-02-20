0f61de5cb4dc64bc5b73f230e0d4aa38f00b5779
Daniel Mateus <u1802833@unimilitar.edu.co>
2020-02-20 01:05:48 -0500

    [add] change photo perfil team

--- views/team.php
+++ views/team.php
@@ -12,7 +12,7 @@
       <div class="row aling-vertical-team">
           <div class=" cont-my-team col-md-4">
             <div class="img-team-perfil">
-                <img src="./img/p2.jpg" alt="" class="img-fluid">
+                <img src="./img/p2.jpeg" alt="" class="img-fluid">
             </div>
             <small class="text-muted">CMO | Chief Marketing Officer</small>
               <h2>Valentina Caballero</h2>
@@ -21,7 +21,7 @@
           </div>
           <div class=" cont-my-team col-md-4">
             <div class="img-team-perfil">
-                <img src="./img/p0.jpg" alt="" class="img-fluid">
+                <img src="./img/p0.jpeg" alt="" class="img-fluid">
             </div>
             <small class="text-muted">CEO | Chief Executive Officer</small>
               <h2>Juan Sebastian Castro</h2>
