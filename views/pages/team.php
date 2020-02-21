9b3c09c67cc32cf8a9b06726b84af69d385f13ef
Daniel Mateus <u1802833@unimilitar.edu.co>
2020-02-20 22:12:35 -0500

    [add] change page team

--- views/pages/team.php
+++ views/pages/team.php
@@ -227,45 +227,17 @@
               '
               <div class="col-lg-9">
 
-              <div class="card mt-4">
+              <div class="card mt-4 mb-5">
                   <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
                   <div class="card-body">
-                      <h3 class="card-title">Product Name</h3>
-                      <h4>$24.99</h4>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit
-                          fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur
-                          praesentium animi perspiciatis molestias iure, ducimus!</p>
-                      <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
-                      4.0 stars
+                      <h3 class="card-title">EntraFácil.com y su equipo</h3>
+                      <p class="card-text">Cuando los proyectos se realizan con amor y entre amigos, Tienen un 100% de posibilidad de crecer, Queremos que tu hagas parte de esta plataforma.</p>
+                      <p class="card-text">Empezamos 3 amigos, cada uno de nosotros, es parte fundamental del equipo y avanzamos todos juntos, si quieres saber algo de nosotros haz clic en nuestros nombres.</p>
+                      <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9733;</span>
+                      Excelente Servicio
                   </div>
               </div>
               <!-- /.card -->
-  
-              <div class="card card-outline-secondary my-4">
-                  <div class="card-header">
-                      Product Reviews
-                  </div>
-                  <div class="card-body">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
-                          similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum.
-                          Sequi mollitia, necessitatibus quae sint natus.</p>
-                      <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                      <hr>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
-                          similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum.
-                          Sequi mollitia, necessitatibus quae sint natus.</p>
-                      <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                      <hr>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
-                          similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum.
-                          Sequi mollitia, necessitatibus quae sint natus.</p>
-                      <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                      <hr>
-                      <a href="#" class="btn btn-success">Habla conmigo</a>
-                  </div>
-              </div>
-              <!-- /.card -->
-  
           </div>
           <!-- /.col-lg-9 -->
   
