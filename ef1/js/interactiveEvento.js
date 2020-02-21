97358f332d2ac8798ba40b6678b490c24c02b093
Daniel Mateus <u1802833@unimilitar.edu.co>
2020-02-20 20:44:48 -0500

    [add] change in aboutus

--- ef1/js/interactiveEvento.js
+++
@@ -1,9 +0,0 @@
-function modificarMarginTop(){
-    var altura = 0;
-    altura = document.getElementById('Header').clientHeight;
-    document.getElementById('mainP').style.marginTop = altura + 'px';
-    console.log(altura + 'px');
-    document.getElementById('Header').classList.add('backgroundScroll');
-}
-
-modificarMarginTop();
--- ef1/js/scrollHeader.js
+++
@@ -1,9 +0,0 @@
-const head = document.getElementById('Header');
-
-window.onscroll = function() {
-    if (window.scrollY >= 20) {
-        head.classList.add('backgroundScroll');
-    } else {
-        head.classList.remove('backgroundScroll');
-    }
-}
